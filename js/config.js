3bb2654d032e207f4b9341a6baba4bb7add4adb6
BlesUstetu <lionustetu@gmail.com>
2026-07-29 20:52:49 +0700

    Add frontend configuration for Guardian KPI

---
+++ js/config.js
@@ -0,0 +1,20 @@
+/**
+ * ==========================================================
+ * Guardian KPI
+ * Frontend Configuration
+ * ==========================================================
+ */
+
+const CONFIG = {
+
+    APP_NAME: "Guardian KPI",
+
+    VERSION: "1.0.0",
+
+    API_URL: "https://script.google.com/macros/s/AKfycbwF2YivVgPsfCg-m797Kzx4eYCsrhqGaX42SCl5LKlOBQZOtp-4cNnrhNgL12JkM92bIQ/exec",
+
+    DEBUG: true,
+
+    THEME: "web3"
+
+};
