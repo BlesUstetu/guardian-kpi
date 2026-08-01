25a61eba109619e9cd9fb3117dce501b044c9080
BlesUstetu <lionustetu@gmail.com>
2026-08-01 10:08:44 +0700

    Refactor API client structure and methods

--- js/api.js
+++ js/api.js
@@ -1,63 +1,319 @@
 /**
  * ==========================================================
- * Guardian KPI API
+ * Guardian KPI Web3
+ * File : js/api.js
+ * ==========================================================
+ * REST API Client
+ * Author : BlesProduction
+ * Version : 2.0.0
  * ==========================================================
  */
 
 const API = {
 
-    async get(action) {
+    BASE_URL: CONFIG.API_URL,
 
-        const res = await fetch(
-            `${CONFIG.API_URL}?action=${action}`
-        );
+    /**
+     * ======================================================
+     * REQUEST
+     * ======================================================
+     */
+    async request(url = "", options = {}) {
 
-        return await res.json();
+        try {
+
+            const response = await fetch(
+                this.BASE_URL + url,
+                {
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    ...options
+                }
+            );
+
+            const json = await response.json();
+
+            return json;
+
+        } catch (err) {
+
+            console.error(err);
+
+            return {
+
+                success: false,
+
+                message: err.message
+
+            };
+
+        }
 
     },
 
-    async post(action, body = {}) {
+    /**
+     * ======================================================
+     * DASHBOARD
+     * ======================================================
+     */
 
-        const res = await fetch(CONFIG.API_URL, {
+    async getDashboard() {
+
+        return this.request("?action=dashboard");
+
+    },
+
+    /**
+     * ======================================================
+     * ANGGOTA
+     * ======================================================
+     */
+
+    async getAnggota() {
+
+        return this.request("?action=anggota");
+
+    },
+
+    async saveAnggota(data) {
+
+        return this.request("", {
 
             method: "POST",
 
-            headers: {
-                "Content-Type": "application/json"
-            },
-
             body: JSON.stringify({
 
-                action,
+                action: "saveAnggota",
 
-                ...body
+                data: data
 
             })
 
         });
 
-        return await res.json();
+    },
+
+    async updateAnggota(id, data) {
+
+        return this.request("", {
+
+            method: "POST",
+
+            body: JSON.stringify({
+
+                action: "updateAnggota",
+
+                id: id,
+
+                data: data
+
+            })
+
+        });
 
     },
 
-    dashboard() {
-        return this.get("dashboard");
+    async deleteAnggota(id) {
+
+        return this.request("", {
+
+            method: "POST",
+
+            body: JSON.stringify({
+
+                action: "deleteAnggota",
+
+                id: id
+
+            })
+
+        });
+
     },
 
-    anggota() {
-        return this.get("anggota");
+    /**
+     * ======================================================
+     * GROUP
+     * ======================================================
+     */
+
+    async getGroup() {
+
+        return this.request("?action=group");
+
     },
 
-    group() {
-        return this.get("group");
+    async saveGroup(data) {
+
+        return this.request("", {
+
+            method: "POST",
+
+            body: JSON.stringify({
+
+                action: "saveGroup",
+
+                data: data
+
+            })
+
+        });
+
     },
 
-    kpi() {
-        return this.get("masterKPI");
+    async updateGroup(id, data) {
+
+        return this.request("", {
+
+            method: "POST",
+
+            body: JSON.stringify({
+
+                action: "updateGroup",
+
+                id: id,
+
+                data: data
+
+            })
+
+        });
+
     },
 
-    penilaian() {
-        return this.get("penilaian");
+    async deleteGroup(id) {
+
+        return this.request("", {
+
+            method: "POST",
+
+            body: JSON.stringify({
+
+                action: "deleteGroup",
+
+                id: id
+
+            })
+
+        });
+
+    },
+
+    /**
+     * ======================================================
+     * MASTER KPI
+     * ======================================================
+     */
+
+    async getMasterKPI() {
+
+        return this.request("?action=masterKPI");
+
+    },
+
+    async saveMasterKPI(data) {
+
+        return this.request("", {
+
+            method: "POST",
+
+            body: JSON.stringify({
+
+                action: "saveMasterKPI",
+
+                data: data
+
+            })
+
+        });
+
+    },
+
+    async updateMasterKPI(id, data) {
+
+        return this.request("", {
+
+            method: "POST",
+
+            body: JSON.stringify({
+
+                action: "updateMasterKPI",
+
+                id: id,
+
+                data: data
+
+            })
+
+        });
+
+    },
+
+    async deleteMasterKPI(id) {
+
+        return this.request("", {
+
+            method: "POST",
+
+            body: JSON.stringify({
+
+                action: "deleteMasterKPI",
+
+                id: id
+
+            })
+
+        });
+
+    },
+
+    /**
+     * ======================================================
+     * PENILAIAN
+     * ======================================================
+     */
+
+    async getPenilaian() {
+
+        return this.request("?action=penilaian");
+
+    },
+
+    async savePenilaian(data) {
+
+        return this.request("", {
+
+            method: "POST",
+
+            body: JSON.stringify({
+
+                action: "savePenilaian",
+
+                data: data
+
+            })
+
+        });
+
+    },
+
+    async deletePenilaian(id) {
+
+        return this.request("", {
+
+            method: "POST",
+
+            body: JSON.stringify({
+
+                action: "deletePenilaian",
+
+                id: id
+
+            })
+
+        });
+
     }
 
 };
