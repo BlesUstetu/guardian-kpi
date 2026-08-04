f5f16df3f2794795248d1b6d6b2a92890f6fc687
BlesUstetu <lionustetu@gmail.com>
2026-08-04 14:27:00 +0700

    Update penilaian.js

--- js/penilaian.js
+++ js/penilaian.js
@@ -1,8 +1,99 @@
 /**
- * Guardian KPI
- * penilaian.js
+ * ==========================================================
+ * Guardian KPI Web3
+ * File : penilaian.js
+ * Version : 1.0.0
+ * ==========================================================
  */
 
-function initPenilaian() {
-    console.log("Penilaian module loaded");
+"use strict";
+
+/* ==========================================================
+ * GLOBAL VARIABLE
+ * ==========================================================
+ */
+
+let penilaianData = [];
+
+let anggotaData = [];
+
+let masterKPIData = [];
+
+let editPenilaianId = null;
+
+/* ==========================================================
+ * INIT
+ * ==========================================================
+ */
+
+async function initPenilaian(){
+
+    clearPenilaianForm();
+
+    await loadAnggotaPenilaian();
+
+    await loadMasterKPI();
+
+    await loadPenilaian();
+
 }
+
+/* ==========================================================
+ * LOAD PENILAIAN
+ * ==========================================================
+ */
+
+async function loadPenilaian(){
+
+    console.log("Load Penilaian");
+
+}
+
+/* ==========================================================
+ * LOAD ANGGOTA
+ * ==========================================================
+ */
+
+async function loadAnggotaPenilaian(){
+
+    console.log("Load Anggota");
+
+}
+
+/* ==========================================================
+ * LOAD MASTER KPI
+ * ==========================================================
+ */
+
+async function loadMasterKPI(){
+
+    console.log("Load Master KPI");
+
+}
+
+/* ==========================================================
+ * CLEAR FORM
+ * ==========================================================
+ */
+
+function clearPenilaianForm(){
+
+    editPenilaianId = null;
+
+}
+
+/* ==========================================================
+ * EXPORT
+ * ==========================================================
+ */
+
+window.initPenilaian = initPenilaian;
+
+window.loadPenilaian = loadPenilaian;
+
+window.loadAnggotaPenilaian = loadAnggotaPenilaian;
+
+window.loadMasterKPI = loadMasterKPI;
+
+window.clearPenilaianForm = clearPenilaianForm;
+
